0e529fc8900f69cb9e612e5962893c6c933a2504
leo-moon <49075905+leo-moon@users.noreply.github.com>
2022-11-30 22:07:12 +0000

    Deploying to gh-pages from @ leo-moon/goit-js-hw-09@8accc6b285020c8ebeb306a1338990fad8f94bf8 🚀

--- 01-color-switcher.eb89dde3.js
+++
@@ -1,2 +0,0 @@
-console.log("hi world!"),refs={body:document.querySelector("body"),btnStart:document.querySelector(dataset=start),btnStop:document.querySelector(dataset=stop)};
-//# sourceMappingURL=01-color-switcher.eb89dde3.js.map
--- 01-color-switcher.f7ba0614.js
+++
@@ -1,2 +0,0 @@
-console.log("hi world!"),refs={body:document.querySelector("body"),btnStart:document.querySelector(dataset=start),btnStop:document.querySelector(dataset=stop)};
-//# sourceMappingURL=01-color-switcher.f7ba0614.js.map
