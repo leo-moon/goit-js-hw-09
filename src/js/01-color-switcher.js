8accc6b285020c8ebeb306a1338990fad8f94bf8
Vladimir Larin <lvv0820@gmail.com>
2022-12-01 00:06:43 +0200

    11

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,30 @@
+import throttle from 'lodash.throttle';
+
 console.log('hi world!')
 
-refs = {
-  body: document.querySelector('body'),
-  btnStart: document.querySelector(dataset=start),
-  btnStop: document.querySelector(dataset=stop)
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector('body'),
+  // btnStart: document.querySelector('dataset start'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]')
+};
+
+refs.btnStart.addEventListener('click', throttle(onBtnStart, 1000));
+refs.btnStop.addEventListener('click', onBtnStop);
+
+function onBtnStart(e) {
+console.log('start');
+refs.btnStart.disabled = true ;
+refs.btnStop.disabled = false ;
+refs.body.style.backgroundColor=getRandomHexColor();
+};
+
+function onBtnStop(e) {
+  console.log('stop');
+  refs.btnStart.disabled = false ;
+  refs.btnStop.disabled = true ;
+};
